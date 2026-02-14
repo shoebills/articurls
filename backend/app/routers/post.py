cea907e06c7f1bce64a0c2d9c5a20ce20736a1a0
Abhishek <abskwdkr@gmail.com>
2026-02-15 02:08:48 +0530

    refactor: exception handling

--- backend/app/routers/post.py
+++ backend/app/routers/post.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models
@@ -10,42 +10,41 @@ router = APIRouter(
     prefix="/post"
 )
 
-@router.post("/", response_model=post.GetPost)
+@router.post("/", response_model=post.GetPost, status_code=status.HTTP_201_CREATED)
 def create_post(request: post.CreatePost, db: Session = Depends(get_db)):
-
     new_post = models.Post(title=request.title, content=request.content)
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
-@router.get("/", response_model=List[post.GetPost])
+@router.get("/", response_model=List[post.GetPost], status_code=200)
 def get_posts(db: Session = Depends(get_db)):
-
     posts = db.query(models.Post).all()
     return posts
 
-@router.get("/{id}", response_model=post.GetPost)
+@router.get("/{id}", response_model=post.GetPost, status_code=200)
 def get_post(id: int, db: Session = Depends(get_db)):
-
     post = db.query(models.Post).filter(models.Post.post_id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
     return post
 
-@router.put("/{id}", response_model=post.GetPost)
+@router.put("/{id}", response_model=post.GetPost, status_code=status.HTTP_202_ACCEPTED)
 def update_post(id: int, request: post.UpdatePost, db: Session = Depends(get_db)):
-
     post = db.query(models.Post).filter(models.Post.post_id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
     post.title = request.title
     post.content = request.content
     db.commit()
     db.refresh(post)
-
     return post
 
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-
-    db.query(models.Post).filter(models.Post.post_id == id).delete()
+    post = db.query(models.Post).filter(models.Post.post_id == id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
+    db.delete(post)
     db.commit()
-
-    return "Deleted post"
--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import models
@@ -9,19 +9,22 @@ router = APIRouter(
     prefix="/user"
 )
 
-@router.post("/", response_model=user.GetUser)
+@router.post("/", response_model=user.GetUser, status_code=status.HTTP_201_CREATED)
 def create_user(request: user.CreateUser, db: Session = Depends(get_db)):
 
+    existing_user = db.query(models.User).filter(models.User.email == request.email).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    
     new_user = models.User(name=request.name, email=request.email, password=request.password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
- 
     return new_user
 
-@router.get("/{id}", response_model=user.GetUser)
+@router.get("/{id}", response_model=user.GetUser, status_code=200)
 def get_user(id: int, db: Session = Depends(get_db)):
-
     user = db.query(models.User).filter(models.User.user_id == id).first()
-
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} doesnt exist")
     return user
