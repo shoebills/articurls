d93315aacd132c52f90cca91e1aeca0b0871c36c
Abhishek <abskwdkr@gmail.com>
2026-02-16 03:12:27 +0530

    feat: login with password verification

--- server/app/hashing.py
+++ server/app/hashing.py
@@ -2,8 +2,8 @@ from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
 
-def get_password_hash(password):
+def get_password_hash(password: str):
     return password_hash.hash(password)
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
     return password_hash.verify(plain_password, hashed_password)
--- server/app/routers/authentication.py
+++ server/app/routers/authentication.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..schemas import authentication, user
-from .. import models
+from .. import models, hashing
 
 router = APIRouter(
     tags=["Authentication"]
@@ -10,10 +10,17 @@ router = APIRouter(
 
 @router.post("/login", response_model=user.GetUser)
 def login(request: authentication.Login, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == request.username, models.User.password == request.password).first()
+
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid Credentials")
+    
+    if not hashing.verify_password(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid Credentials")
     
     return user
