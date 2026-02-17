e27f6a9ada283bc657c836468feccfc3c8b879f6
Abhishek <abskwdkr@gmail.com>
2026-02-18 00:30:39 +0530

    chore: updates in requirements file

--- server/app/security/oauth2.py
+++ server/app/security/oauth2.py
@@ -34,7 +34,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
+        email: str = payload.get("sub")
 
         if email is None:
             raise HTTPException(
