8f4c3a7682c3a212fcdb94416a27ca6622e27330
Abhishek <abskwdkr@gmail.com>
2026-02-28 22:27:11 +0530

    very little changes

--- server/app/models.py
+++ server/app/models.py
@@ -9,6 +9,7 @@ class User(Base):
 
     user_id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    user_name = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     
@@ -16,7 +17,7 @@ class Blog(Base):
     __tablename__ = "blogs"
 
     blog_id = Column(Integer, primary_key=True)
+    user_id = Column(ForeignKey("users.user_id"), nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
-    user_id = Column(ForeignKey("users.user_id"), nullable=False)
