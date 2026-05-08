a23334fa214398bda4b1b52f00eeee3042fafe38
Abhishek <abskwdkr@gmail.com>
2026-05-08 19:37:15 +0530

    fix

---
+++ alembic/versions/n1o2p3q4r5s6_merge_page_status_and_preview_image_heads.py
@@ -0,0 +1,22 @@
+"""merge page status and preview image heads
+
+Revision ID: n1o2p3q4r5s6
+Revises: l2m3n4o5p6q7, m7n8o9p0q1r2
+Create Date: 2026-05-08
+
+"""
+from alembic import op
+
+
+revision = "n1o2p3q4r5s6"
+down_revision = ("l2m3n4o5p6q7", "m7n8o9p0q1r2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
