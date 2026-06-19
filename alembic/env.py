13bc592e02ac72ab134a766fb4d86c0522092be7
Abhishek <abskwdkr@gmail.com>
2026-06-19 22:27:59 +0530

    fix: db connection url fix

--- alembic/env.py
+++ alembic/env.py
@@ -1,24 +1,24 @@
 from logging.config import fileConfig
 from urllib.parse import quote_plus
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 from src.app.models import Base
 from src.app.config import settings
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
-config.set_main_option(
-    "sqlalchemy.url",
+url = (
     f"postgresql+psycopg://{quote_plus(settings.database_username)}:"
     f"{quote_plus(settings.database_password)}@"
     f"{settings.database_hostname}:{settings.database_port}/"
     f"{settings.database_name}"
-    f"{'?sslmode=' + settings.database_sslmode if settings.database_sslmode else ''}",
+    f"{'?sslmode=' + settings.database_sslmode if settings.database_sslmode else ''}"
 )
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -38,18 +38,6 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -62,17 +50,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(
