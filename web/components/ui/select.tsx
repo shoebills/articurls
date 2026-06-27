a8f52736191bbedb6c0293e2a77bf003cf14a804
Abhishek <abskwdkr@gmail.com>
2026-06-27 15:10:25 +0530

    Fix dropdown final ig

--- web/components/ui/select.tsx
+++ web/components/ui/select.tsx
@@ -104,7 +104,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 min-w-0",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 min-w-0 [&>span]:min-w-0",
       className
     )}
     {...props}
@@ -114,8 +114,8 @@ const SelectItem = React.forwardRef<
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
-    <SelectPrimitive.ItemText className="block min-w-0 truncate">
-      {children}
+    <SelectPrimitive.ItemText>
+      <span className="block truncate">{children}</span>
     </SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
 ));
